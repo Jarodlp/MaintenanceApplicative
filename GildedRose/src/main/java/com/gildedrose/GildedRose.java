395269ebc0143f944ff90ccf1ddf990641484df6
Jarod Toussaint <jarod.toussaint8@etu.univ-lorraine.fr>
2025-03-05 09:22:15 +0100

    Refactoring handleSulfuras

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -17,57 +17,59 @@ class GildedRose {
         }
     }
 
-    public void handleItem(Item item){
-        if (!item.name.equals(AGED_BRIE)
-                && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-            if (item.quality > 0) {
-                if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                    item.quality = item.quality - 1;
-                }
-                if (item.name.equals(CONJURED)) {
-                    item.quality = item.quality - 1;
-                }
-            }
+    public void handleItem(Item item) {
+        if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            handleSulfuras(item);
         } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-
-                if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+            if (!item.name.equals(AGED_BRIE)
+                    && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                    if (item.name.equals(CONJURED)) {
+                        item.quality = item.quality - 1;
                     }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-            item.sellIn = item.sellIn - 1;
-        }
-
-        if (item.sellIn < 0) {
-            if (!item.name.equals(AGED_BRIE)) {
-                if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = item.quality - item.quality;
                 }
             } else {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
+
+                    if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            item.sellIn = item.sellIn - 1;
+
+            if (item.sellIn < 0) {
+                if (!item.name.equals(AGED_BRIE)) {
+                    if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                        if (item.quality > 0) {
+                            item.quality = item.quality - 1;
+                        }
+                    } else {
+                        item.quality = item.quality - item.quality;
+                    }
+                } else {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
                 }
             }
         }
+    }
+
+    void handleSulfuras(Item item) {
+
     }
 }
