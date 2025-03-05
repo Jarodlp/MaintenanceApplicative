7103db15c937bd2e64c410dae8ef03c9b9a52640
Jarod Toussaint <jarod.toussaint8@etu.univ-lorraine.fr>
2025-03-05 09:52:17 +0100

    Refactoring handleBackstage, tous les items handle

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -24,40 +24,18 @@ class GildedRose {
             handleConjured(item);
         } else if (item.name.equals(AGED_BRIE)) {
             handleAgedBrie(item);
+        } else if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+            handleBackstage(item);
         } else {
-            if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
 
             item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                } else {
-                    item.quality = item.quality - item.quality;
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             }
         }
@@ -98,6 +76,33 @@ class GildedRose {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
             }
+        }
+    }
+
+    void handleBackstage(Item item) {
+
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            item.quality = item.quality - item.quality;
         }
     }
 }
