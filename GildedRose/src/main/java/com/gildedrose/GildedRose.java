deaa75d53d52c4c0498162a6cb06c0cab9b9aaad
Jarod Toussaint <jarod.toussaint8@etu.univ-lorraine.fr>
2025-03-06 08:21:32 +0100

    Changement handleBackstage pour le sellIn

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -87,6 +87,8 @@ class GildedRose {
 
     void handleBackstage(Item item) {
 
+        item.sellIn = item.sellIn - 1;
+
         if (item.quality < 50) {
             item.quality = item.quality + 1;
 
@@ -103,10 +105,8 @@ class GildedRose {
             }
         }
 
-        item.sellIn = item.sellIn - 1;
-
         if (item.sellIn < 0) {
-            item.quality = item.quality - item.quality;
+            item.quality = 0;
         }
     }
 }
